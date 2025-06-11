338595bb2ca4cd55ae3723aac2ee418ba615f021
Manfred Ng <manfred@giftandgo.com>
2025-06-11 15:29:21 +0100

    BAEL-9331: Chat Memory in Spring AI

---
+++ spring-ai-4/src/main/java/com/baeldung/springai/memory/ChatService.java
@@ -0,0 +1,39 @@
+package com.baeldung.springai.memory;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.UUID;
+
+@Component
+@SessionScope
+public class ChatService {
+
+    private final ChatClient chatClient;
+    private final String conversationId;
+
+    public ChatService(ChatModel chatModel, ChatMemory chatMemory) {
+        this.chatClient = ChatClient.builder(chatModel)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
+        this.conversationId = UUID.randomUUID().toString();
+    }
+
+    public String chat(String prompt) {
+        return chatClient.prompt()
+                .system(systemMessage -> systemMessage
+                        .text("Format the output in HTML")
+                        .text("You can use <b>bold</b> and <i>italics</i> tags")
+                        .text("Use HTML table to format the output if you are asked for comparisons on multiple stuff")
+                )
+                .user(userMessage -> userMessage.text(prompt))
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .call()
+                .content();
+    }
+
+}
